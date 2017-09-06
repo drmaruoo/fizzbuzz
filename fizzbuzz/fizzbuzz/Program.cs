4163a6dadc3221442af2b79c2204941ce3727b54
Marcin Domarski <31709302+drmaruoo@users.noreply.github.com>
2017-09-06 22:57:35 +0200

    created the universe (i mean everything)

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input max value that will be fizzbuzzed: ");
+            int max = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine("");
+                if ((i + 1) % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if ((i + 1) % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                if ((i + 1) % 3 != 0 && (i + 1) % 5 != 0)
+                {
+                    Console.Write(i + 1);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
